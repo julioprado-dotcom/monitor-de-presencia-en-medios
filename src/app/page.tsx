fa96358d7489ebc4e6b541651717828ab829ccb9
Z User <z@container>
2026-05-15 20:55:08 +0000

    358602ea-0202-4cf9-b28e-f692fe56486d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 'use client';
 
 import { Suspense } from 'react';
-import dynamic from 'next/dynamic';
+import { NewDashboard } from '@/components/dashboard/NewDashboard';
 import { LoadingScreen } from '@/components/dashboard/LoadingScreen';
-
-const NewDashboard = dynamic(
-  () => import('@/components/dashboard/NewDashboard'),
-  { ssr: false, loading: () => <LoadingScreen /> }
-);
 
 export default function AdminPage() {
   return (
